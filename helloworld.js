0b6e84cc8544b81ea16e6935b78dbe6d368567d5
antonius-ras <antonius.19115@gmail.com>
2026-01-18 15:55:15 +0700

    On branch main

---
+++ helloworld.js
@@ -0,0 +1,10 @@
+// กำหนดตัวแปรสำหรับเก็บข้อมูลส่วนตัว
+let name = "Tonias ras";
+let id = 66110053;
+let age = 21;
+let mejor = "วิศวกรรมคอมพิวเตอร์";
+// แสดงข้อมูลส่วนตัวบนคอนโซล
+console.log("Name: " + name);
+console.log("ID: " + id);
+console.log("Age: " + age);
+console.log("Major: " + mejor);
